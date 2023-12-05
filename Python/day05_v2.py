f57d24e81fe05c9063ad3546af3b3a0d845e943f
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2023-12-05 11:32:32 +0100

    Day 5 AOC 2023

--- Python/day05_v2.py
+++ Python/day05_v2.py
@@ -6,14 +6,6 @@ data_file = open('../Data/day05.txt', 'r')
 data_lines = [line for line in data_file]
 data_file.close()
 
-
-seed_ranges = [int(num) for num in data_lines[0][7:-1].split(' ')]
-seeds = []
-for i in range(0,len(seed_ranges),2) :
-    for j in range(seed_ranges[i],seed_ranges[i]+seed_ranges[i+1]) :
-        seeds.append(j)
-print(len(seeds))
-
 l = 3
 data_sets = []
 while l < len(data_lines):
@@ -25,18 +17,31 @@ while l < len(data_lines):
     data_sets.append(transform)
     l += i+2
 
-def get_transform(seed) :
-    location = seed
+def get_inverse_transform(location) :
+    seed = location
     global data_sets
-    for transform in data_sets :
+    for t in range(len(data_sets)-1,-1,-1) :
+        transform = data_sets[t]
         i = 0
         while i < len(transform) :
-            if transform[i][1] <= location < transform[i][1]+transform[i][2] :
-                location += transform[i][0] - transform[i][1]
+            if transform[i][0] <= seed < transform[i][0]+transform[i][2] :
+                seed += transform[i][1] - transform[i][0]
                 break
             i += 1
-    return location
+    return seed
 
-seeds_to_location = [get_transform(seed) for seed in seeds]
-print('Result = ', min(seeds_to_location))
-# Answer is 3374647
+
+seed_ranges = [int(num) for num in data_lines[0][7:-1].split(' ')]
+
+location = 0
+a = 0
+while a == 0 :
+    seed = get_inverse_transform(location)
+    for i in range(0,len(seed_ranges),2) :
+        if seed_ranges[i] <= seed and seed < seed_ranges[i]+seed_ranges[i+1] :
+            a = 1
+            break
+    location += 1
+    
+print('Result = ', location-1)
+# Answer is 
