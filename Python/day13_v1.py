5fb9a6847b688d2c52ab16bb29ac54626a82d10c
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2023-12-13 09:36:39 +0100

    Day 13 Part 1 AOC 2023

---
+++ Python/day13_v1.py
@@ -0,0 +1,61 @@
+# Day 13, part 1 of Advent of Code 2023
+
+data_file = open('./Data/day13_test1.txt', 'r')
+
+#data_file = open('./Data/day13.txt', 'r')
+
+data_lines = [line.strip() for line in data_file]
+data_file.close()
+
+patterns = []
+i = 0
+while i < len(data_lines):
+    pattern = []
+    while i < len(data_lines) and data_lines[i] != '':
+        pattern.append(data_lines[i])
+        i += 1
+    patterns.append(pattern)
+    i += 1
+
+def get_reflections(pattern):
+    reflections_rows = {}
+    i = 0
+    while i < len(pattern)-1:
+        if pattern[i][:] == pattern[i+1][:] :
+            reflections_rows[i] = 1
+        i += 1
+    for r in reflections_rows :
+        length = 1
+        while r-length >= 0 and r+length+1 < len(pattern) and pattern[r-length][:] == pattern[r+length+1][:] :
+            length += 1
+    print('Rows',reflections_rows)
+    
+    reflections_columns = {}
+    return reflections_rows, reflections_columns
+
+
+
+m_rows = 100
+result = 0
+for pattern in patterns :
+    reflections_rows, reflections_columns = get_reflections(pattern)
+    max_r = 0
+    reflection = -1
+    column = False
+    for r in reflections_rows:
+        if reflections_rows[r] > max_r:
+            max_r = reflections_rows[r]
+            reflection = r
+    for r in reflections_columns:
+        if reflections_columns[r] > max_r:
+            max_r = reflections_columns[r]
+            reflection = r
+            column = True
+    #print('Pattern',p,'Reflection',reflection,'Column',column)
+    if column :
+        result += reflection + 1
+    else :
+        result += (reflection + 1) * m_rows
+ 
+print('Result = ',result)
+# Answer is 7286
