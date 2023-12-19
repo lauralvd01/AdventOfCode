c180ed4a201fd85d40b9219de5abf01eb254f9fc
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2023-12-19 16:54:55 +0100

    Day 19 Part 2 AOC 2023 Attempts

--- Python/day19_v2.py
+++ Python/day19_v2.py
@@ -15,19 +15,24 @@ class Rule :
             self.compare = int(rule.split(':')[0].split(self.test_sign)[1])
             self.destination = rule.split(':')[1]
         else :
-            self.attribute = None
+            self.attribute = True
             self.test_sign = None
             self.compare = None
             self.destination = rule
     
     def __repr__(self) :
-        if self.attribute == None :
-            return f' => {self.destination}'
+        if self.test_sign == None :
+            return f'{self.attribute} => {self.destination}'
         return f'if {self.attribute} {self.test_sign} {self.compare} => {self.destination}'
     
+    def get_str(self) :
+        if self.test_sign == None :
+            return self.destination
+        return f'{self.attribute}{self.test_sign}{self.compare}:{self.destination}'
+    
     def test(self, rating) :
-        if self.attribute == None :
-            return True
+        if self.test_sign == None :
+            return self.attribute
         if self.test_sign == '>' :
             if self.attribute == 'x' :
                 return rating.x > self.compare
@@ -48,6 +53,18 @@ class Rule :
                 return rating.s < self.compare
         return False
 
+    def get_inv(self) :
+        if self.test_sign == None :
+            return not self.attribute
+        if self.test_sign == '<' :
+            new_compare = self.compare - 1
+            new_test_sign = '>'
+        if self.test_sign == '>' :
+            new_compare = self.compare + 1
+            new_test_sign = '<'
+        strr = f'{self.attribute}{new_test_sign}{new_compare}:{self.destination}'
+        return Rule(strr)
+        
 
 class Workflow :
     def __init__(self, line) :
@@ -58,6 +75,16 @@ class Workflow :
     
     def __repr__(self) :
         return f'{self.name} : {self.rules}'
+    
+    def get_destinations(self) :
+        return [rule.destination for rule in self.rules]
+    
+    def find(self,dest) :
+        destinations = self.get_destinations()
+        for i in range(len(destinations)) :
+            if destinations[i] == dest :
+                return i
+        return -1
 
 workflows = {}
 l = 0
@@ -66,46 +93,204 @@ while data_lines[l] != '' :
     workflows[wk.name] = wk
     l += 1
 
+workflows_with_A = []
+for wk in workflows :
+    if 'A' in workflows[wk].get_destinations() :
+        workflows_with_A.append(workflows[wk])
 
-class Rating :
-    def __init__(self, line) :
-        attributes = line.split('{')[1].split('}')[0].split(',')
-        self.x = int(attributes[0].split('=')[1])
-        self.m = int(attributes[1].split('=')[1])
-        self.a = int(attributes[2].split('=')[1])
-        self.s = int(attributes[3].split('=')[1])
-        self.state = 'in'
-    
-    def __repr__(self) :
-        return f'(x={self.x}, m={self.m}, a={self.a}, s={self.s}) => {self.state}'
-    
-    def __eq__(self, other) :
-        return self.x == other.x and self.m == other.m and self.a == other.a and self.s == other.s
-    
-    def score(self) :
-        return self.x + self.m + self.a + self.s
-    
-    def sort(self) :
-        rules = workflows[self.state].rules
-        for rule in rules :
-            if rule.test(self) :
-                self.state = rule.destination
-                break
+def compatible(rules,add_rules) :
+    comp = True
+    for rule in rules :
+        if rule.test_sign != None :
+            if rule.attribute == 'x' :
+                for add_rule in add_rules :
+                    if add_rule.attribute == 'x' :
+                        if rule.test_sign == '<' and add_rule.test_sign == '>' :
+                            comp = comp and rule.compare >= add_rule.compare
+                        elif rule.test_sign == '>' and add_rule.test_sign == '<' :
+                            comp = comp and rule.compare <= add_rule.compare
+                        #print(rule,add_rule,comp)
+            elif rule.attribute == 'm' :
+                for add_rule in add_rules :
+                    if add_rule.attribute == 'm' :
+                        if rule.test_sign == '<' and add_rule.test_sign == '>' :
+                            comp = comp and rule.compare >= add_rule.compare
+                        elif rule.test_sign == '>' and add_rule.test_sign == '<' :
+                            comp = comp and rule.compare <= add_rule.compare
+                        #print(rule,add_rule,comp)
+            elif rule.attribute == 'a' :
+                for add_rule in add_rules :
+                    if add_rule.attribute == 'a' :
+                        if rule.test_sign == '<' and add_rule.test_sign == '>' :
+                            comp = comp and rule.compare >= add_rule.compare
+                        elif rule.test_sign == '>' and add_rule.test_sign == '<' :
+                            comp = comp and rule.compare <= add_rule.compare
+                        #print(rule,add_rule,comp)
+            elif rule.attribute == 's' :
+                for add_rule in add_rules :
+                    if add_rule.attribute == 's' :
+                        if rule.test_sign == '<' and add_rule.test_sign == '>' :
+                            comp = comp and rule.compare >= add_rule.compare
+                        elif rule.test_sign == '>' and add_rule.test_sign == '<' :
+                            comp = comp and rule.compare <= add_rule.compare
+                        #print(rule,add_rule,comp)
+    #print(comp)
+    return comp
 
-ratings = []
-l += 1
-while l < len(data_lines) :
-    ratings.append(Rating(data_lines[l]))
-    l += 1
+def new_rules(rules,add_rules) :
+    add_rules_copy = add_rules.copy()
+    new_rules = []
+    for rule in rules :
+        if rule.test_sign != None :
+            if rule.attribute == 'x' :
+                find_other_x = False
+                for add_rule in add_rules_copy :
+                    if add_rule.attribute == 'x' :
+                        find_other_x = True
+                        if rule.test_sign == add_rule.test_sign :
+                            if rule.test_sign == '<' :
+                                if rule.compare <= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                            else :
+                                if rule.compare >= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                        else :
+                            if rule not in new_rules :
+                                new_rules.append(rule)
+                            if add_rule not in new_rules :
+                                new_rules.append(add_rule)
+                        add_rules_copy.remove(add_rule)
+                if not find_other_x :
+                    new_rules.append(rule)
+            elif rule.attribute == 'm' :
+                find_other_m = False
+                for add_rule in add_rules_copy :
+                    if add_rule.attribute == 'm' :
+                        find_other_m = True
+                        if rule.test_sign == add_rule.test_sign :
+                            if rule.test_sign == '<' :
+                                if rule.compare <= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                            else :
+                                if rule.compare >= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                        else :
+                            if rule not in new_rules :
+                                new_rules.append(rule)
+                            if add_rule not in new_rules :
+                                new_rules.append(add_rule)
+                        add_rules_copy.remove(add_rule)
+                if not find_other_m :
+                    new_rules.append(rule)
+            elif rule.attribute == 'a' :
+                find_other_a = False
+                for add_rule in add_rules_copy :
+                    if add_rule.attribute == 'a' :
+                        find_other_a = True
+                        if rule.test_sign == add_rule.test_sign :
+                            if rule.test_sign == '<' :
+                                if rule.compare <= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                            else :
+                                if rule.compare >= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                        else :
+                            if rule not in new_rules :
+                                new_rules.append(rule)
+                            if add_rule not in new_rules :
+                                new_rules.append(add_rule)
+                        add_rules_copy.remove(add_rule)
+                if not find_other_a :
+                    new_rules.append(rule)
+            elif rule.attribute == 's' :
+                find_other_s = False
+                for add_rule in add_rules_copy :
+                    if add_rule.attribute == 's' :
+                        find_other_s = True
+                        if rule.test_sign == add_rule.test_sign :
+                            if rule.test_sign == '<' :
+                                if rule.compare <= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                            else :
+                                if rule.compare >= add_rule.compare :
+                                    new_rules.append(rule)
+                                else :
+                                    new_rules.append(add_rule)
+                        else :
+                            if rule not in new_rules :
+                                new_rules.append(rule)
+                            if add_rule not in new_rules :
+                                new_rules.append(add_rule)
+                        add_rules_copy.remove(add_rule)
+                if not find_other_s :
+                    new_rules.append(rule)
+    for rule in add_rules_copy :
+        if rule not in new_rules :
+            new_rules.append(rule)
+    return new_rules
 
-A = []
-for rating in ratings :
-    while rating.state != 'A' and rating.state != 'R' :
-        rating.sort()
-        #print(rating)
-    if rating.state == 'A' :
-        A.append(rating)
+paths = []
+for wk in workflows_with_A :
+    index = wk.find('A')
+    if index >= 0 :
+        rules_to_follow = [rule.get_inv() for rule in wk.rules[:index]]
+        rules_to_follow.append(wk.rules[index])
+        paths.append([wk.name,rules_to_follow])
+    #print(paths[-1])
 
+correct_ratings = []
+while len(paths) > 0 :
+    path = paths[0]
+    for wk in workflows :
+        index = workflows[wk].find(path[0])
+        if index >= 0 :
+            rules_to_follow = [rule.get_inv() for rule in workflows[wk].rules[:index]]
+            rules_to_follow.append(workflows[wk].rules[index])
+            if compatible(path[1],rules_to_follow) :
+                new_path = [workflows[wk].name,new_rules(path[1],rules_to_follow)]
+                #print(new_path)
+                if new_path[0] != 'in' :
+                    paths.append(new_path)
+                else : 
+                    correct_ratings.append(new_path)
+            paths.remove(path)
+        
+def nb_possibilities(correct_rules) :
+    nb = 1
+    for attribute in ['x','m','a','s'] :
+        if attribute not in [rule.attribute for rule in correct_rules] :
+            nb *= 4000
+        else :
+            rules = []
+            for rule in correct_rules :
+                if rule.attribute == attribute :
+                    rules.append(rule)
+            if len(rules) == 1 :
+                if rules[0].test_sign == '<':
+                        nb *= rules[0].compare -1
+                elif rules[0].test_sign == '>':
+                    nb *= 4000 - rules[0].compare
+            else :
+                if rules[0].test_sign == '<' :
+                    nb *= rules[0].compare - rules[1].compare - 1
+                elif rules[0].test_sign == '>' :
+                    nb *= rules[1].compare - rules[0].compare - 1
+    print(correct_rules,nb)
+    return nb
 
-print('\nResult = ',sum([rating.score() for rating in A]))
-# Answer is 495298
+print('\nResult = ',sum([nb_possibilities(correct_rating[1]) for correct_rating in correct_ratings]))
+# Answer is 167409079868000 for the test
