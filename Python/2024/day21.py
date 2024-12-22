45d36ff13bb28189b450b11878fbbe79ff0fdbdf
Laura Levraud <laura.levraud1@etu.univ-lorraine.fr>
2024-12-22 12:21:04 +0100

    Day 21 - 2024

--- Python/2024/day21.py
+++ Python/2024/day21.py
@@ -1,9 +1,11 @@
 # Day 21 - Advent Of Code 2024
 
+from itertools import permutations
+
 # data_file = open("C:\\Users\\LauraLvd\\Documents\\PRO\\AdventOfCode\\Data\\2024\\day21_test.txt", 'r')
 data_file = open("C:\\Users\\LauraLvd\\Documents\\PRO\\AdventOfCode\\Data\\2024\\day21.txt", 'r')
 
-part = 1
+part = 2
 
 # +---+---+---+
 # | 7 | 8 | 9 |
@@ -95,6 +97,98 @@ def robot_to_robot(code) :
         start_position = end_position
     return sequences
 
+############################################################################################### # PART 2
+
+def robot_to_numerical_2(code) :
+    numerical_tabloid = [['7','8','9'],['4','5','6'],['1','2','3'],['X','0','A']]
+    sequences = []
+    start_position = numerical_positions['A']
+    for button_to_reach in code :
+        sequences.append([])
+        
+        end_position = numerical_positions[button_to_reach]
+        di, dj = end_position[0]-start_position[0], end_position[1]-start_position[1]
+        sequence = ('v'*di if di >= 0 else '^'*abs(di)) + ('>'*dj if dj >= 0 else '<'*abs(dj))
+        
+        for seq in permutations(sequence) :
+            if (''.join(seq)+'A') not in sequences[-1] :
+                current = start_position
+                for s in seq :
+                    if s == '^' :
+                        current = (current[0]-1,current[1])
+                    if s == 'v' :
+                        current = (current[0]+1,current[1])
+                    if s == '>' :
+                        current = (current[0],current[1]+1)
+                    if s == '<' :
+                        current = (current[0],current[1]-1)
+                    if not (0 <= current[0] < len(numerical_tabloid) and 0 <= current[1] < len(numerical_tabloid[1])) or numerical_tabloid[current[0]][current[1]] == 'X' :
+                        break
+                if current == end_position :
+                    sequences[-1].append(''.join(seq)+'A')
+                    
+        start_position = end_position
+    return sequences
+
+def robot_to_robot_2(code) :
+    robot_tabloid = [['X','^','A'],['<','v','>']]
+    sequences = []
+    start_position = robot_positions['A']
+    for button_to_reach in code :
+        sequences.append([])
+        
+        end_position = robot_positions[button_to_reach]
+        di, dj = end_position[0]-start_position[0], end_position[1]-start_position[1]
+        sequence = ('v'*di if di >= 0 else '^'*abs(di)) + ('>'*dj if dj >= 0 else '<'*abs(dj))
+        
+        for seq in permutations(sequence) :
+            if (''.join(seq)+'A') not in sequences[-1] :
+                current = start_position
+                for s in seq :
+                    if s == '^' :
+                        current = (current[0]-1,current[1])
+                    if s == 'v' :
+                        current = (current[0]+1,current[1])
+                    if s == '>' :
+                        current = (current[0],current[1]+1)
+                    if s == '<' :
+                        current = (current[0],current[1]-1)
+                    if not (0 <= current[0] < len(robot_tabloid) and 0 <= current[1] < len(robot_tabloid[1])) or robot_tabloid[current[0]][current[1]] == 'X' :
+                        break
+                if current == end_position :
+                    sequences[-1].append(''.join(seq)+'A')
+                    
+        start_position = end_position
+    return sequences
+
+def count(step_to_perform, robot_repetitions) :
+    # step_to_perform = [ possible paths to perform one step of the numerical code or previous robot sequence ]
+    # robot_repetitions = number of robots to perform the code or previous robot sequence
+    
+    counts = [] # [ moves count for each possible path for this step ]
+    for sequence in step_to_perform :
+        # sequence = one possible path to perform this step
+        possible_paths_by_sequence_step = robot_to_robot_2(sequence)
+        # [ [ possible robot paths for sequence[0] ], [ possible robot paths for sequence[1] ], ... ]
+        
+        min_counts = [] # [ min last robot moves count to perform sequence[i] ]
+        for sequence_step in possible_paths_by_sequence_step :
+            # sequence_step = [ possible robot paths for sequence[i] ]
+            if robot_repetitions == 1 :
+                # Last robot to perform the sequence
+                min_counts.append(min([len(path) for path in sequence_step]))
+            else :
+                # More robots to perform the sequence
+                min_counts.append(count(sequence_step, robot_repetitions-1))
+        counts.append(sum(min_counts))
+    return min(counts) # Minimum last robot moves count to perform this step
+
+#     +---+---+
+#     | ^ | A |
+# +---+---+---+
+# | < | v | > |
+# +---+---+---+
+
 def solve(part) :
     global directions
     
@@ -121,12 +215,26 @@ def solve(part) :
             total += min_lenght*int(code[:3])
             print(total)
             
-        return total # Answer is 
+        return total # Answer is 94426
     
     elif part == 2 :
         # Part 2
         
+        numerical_codes = data_file.read().split('\n')
+        data_file.close()
+        
         total = 0
+        for code in numerical_codes :
+            print(code)
+            possible_sequences_by_step = robot_to_numerical_2(code)
+            # [ [ possible robot paths for code[0] ], [ possible robot paths for code[1] ], ... ]
+            
+            counts = [count(step,25) for step in possible_sequences_by_step]
+            print(sum(counts))
+            total += sum(counts)*int(code[:3])
+            print(total)
+            print()
+        
         return total # Answer is 
     
     else:
