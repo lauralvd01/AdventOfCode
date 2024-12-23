8739c91eda1cdc42b6b84f0f95632c840e8dfda7
Laura Levraud <laura.levraud1@etu.univ-lorraine.fr>
2024-12-23 09:34:01 +0100

    Day 22 - 2024

---
+++ Python/2024/day22.py
@@ -0,0 +1,29 @@
+# Day 22 - Advent Of Code 2024
+
+data_file = open("C:\\Users\\LauraLvd\\Documents\\PRO\\AdventOfCode\\Data\\2024\\day22_test.txt", 'r')
+# data_file = open("C:\\Users\\LauraLvd\\Documents\\PRO\\AdventOfCode\\Data\\2024\\day22.txt", 'r')
+
+part = 1
+
+def solve(part) :
+    if part == 1 :
+        # Part 1
+        
+        data_file.close()
+        
+        total = 0
+        return total # Answer is 
+    
+    elif part == 2 :
+        # Part 2
+        
+        data_file.close()
+        
+        total = 0        
+        return total # Answer is 
+    
+    else:
+        data_file.close()
+        return 'Invalid part'
+
+print(solve(part))
